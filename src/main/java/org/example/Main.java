fd0d17a14791e6f07a5fa46bab121a4dd4c55453
shadeisroot <dschristensen@proton.me>
2024-03-22 10:36:31 +0100

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Student> Studentlist = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("hey");
+                break;
+            case 2:
+                // Perform "encrypt number" case.
+                break;
+            case 3:
+                // Perform "decrypt number" case.
+                break;
+            case 4:
+                // Perform "quit" case.
+                break;
+            default:
+                // The user input an unexpected choice.
+        }
     }
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Student {
+
+    private String studentid;
+    private String Firstname;
+    private String Lastname;
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        Firstname = firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String lastname) {
+        Lastname = lastname;
+    }
+
+    public Student(String studentid, String firstname, String lastname) {
+        this.studentid = studentid;
+        Firstname = firstname;
+        Lastname = lastname;
+    }
+}
