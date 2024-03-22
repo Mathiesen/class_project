1a3a213c58c495939d042e9baf0fb2f78702f3d0
Jakoc <damernesjacob@gmail.com>
2024-03-22 10:48:37 +0100

    mojne

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -4,6 +4,7 @@ public class StudentManager {
 
 
     public void updateStudentName(String oldName, String newName){
+        //vi har fucking antaget er der en student klasse og en liste med students
         for (Student student : students) {
             if (student.getName().equals(oldName)) {
                 student.updateName(newName);
@@ -13,6 +14,7 @@ public class StudentManager {
     }
 
     public void updateStudentAge(String name, int newAge){
+        //vi har fucking antaget er der en student klasse og en liste med students
         for (Student student : students) {
             if (student.getAge().equals(oldAge)) {
                 student.updateAge(newAge);
