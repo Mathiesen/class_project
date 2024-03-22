2656beb6c3a87e2960c2ea43990935419e7f5854
andhot01 <andra.hotnogu@yahoo.com>
2024-03-22 10:48:57 +0100

    extra sout

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -4,5 +4,6 @@ public class StudentManager {
     DataAccess da = new DataAccess();
     public void deleteStudent(){
         da.delFromImaginaryDB();
+        System.out.println("Calling database...");
     }
 }
