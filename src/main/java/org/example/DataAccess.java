90dd09a72e57278b06a85c75b91ada97fb2a3baf
andhot01 <andra.hotnogu@yahoo.com>
2024-03-22 10:37:40 +0100

    Delete from imaginary db method i data access

--- src/main/java/org/example/DataAccess.java
+++ src/main/java/org/example/DataAccess.java
@@ -1,4 +1,13 @@
 package org.example;
 
 public class DataAccess {
+    public void delFromImaginaryDB(){
+        String preparedStatement = "DELETE Student FROM ListOfStudent WHERE Id = ?";
+        int ctrl = 0; //skal forestille try and catch på en db
+        if (ctrl == 0){
+            System.out.println("Student deleted" + preparedStatement);
+        } else {
+            System.out.println("Couldn't find student");
+        }
+    }
 }
